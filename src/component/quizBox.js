34c22f3b42d465a3a8235cec6f94fd45f09a54a7
rushanthbala <rushanth9999@gmail.com>
2021-03-30 22:56:59 +0530

    addHint functions

--- src/component/quizBox.js
+++ src/component/quizBox.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import Questions from "../api/question.json";
 import M from "materialize-css";
 import "./quizBox.css";
-import Button from "./customise/Button";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
 import TimerIcon from "@material-ui/icons/Timer";
@@ -27,23 +26,23 @@ export default function Quiz() {
   const [FityFity, setFityFity] = useState(2);
   // const [UseFityFity, setUseFityFity] = useState(0);
   // const [Time, setTime] = useState({});
+  const [priviosRandomNo, setPriviosRandomNo] = useState([])
   const onButtonClick = (e) => {
-      console.log(e.target.id);
-      switch (e.target.id) {
-      case 'next':
-      let nextQuestion = currentQuestionIndex + 1;
-          // setCurrentQuestionIndex(currentQuestionIndex1)  
-          setCurrentQuestionIndex(nextQuestion);
-          setCurrentQuestion(question[nextQuestion]);
-          setAnswer(question[nextQuestion].answer);
-          setNumberQuestion(numberQuestion + 1);     
+    showHiddenOption();
+    switch (e.target.id) {
+      case "next":
+        let nextQuestion = currentQuestionIndex + 1;
+        setCurrentQuestionIndex(nextQuestion);
+        setCurrentQuestion(question[nextQuestion]);
+        setAnswer(question[nextQuestion].answer);
+        setNumberQuestion(numberQuestion + 1);
         break;
-        case 'previos':
-          previosQuestion()
+      case "previos":
+        previosQuestion();
         break;
-    
+
       default:
-      console.log(currentQuestion);
+        console.log(currentQuestion);
 
         break;
     }
@@ -54,21 +53,21 @@ export default function Quiz() {
       document.getElementById("buttonQuestionSound").play();
     }, 400);
   };
-const previosQuestion =() =>{
-  if (currentQuestionIndex==0 ) {
-    alert('no privou')
-  }else {
-    let previosQuestion = currentQuestionIndex - 1;
-    // setCurrentQuestionIndex(currentQuestionIndex1)  
-    setCurrentQuestionIndex(previosQuestion);
-    setCurrentQuestion(question[previosQuestion]);
-    setAnswer(question[previosQuestion].answer);
-    setNumberQuestion(numberQuestion + 1);  
-  }
-    
-}
+  const previosQuestion = () => {
+    if (currentQuestionIndex == 0) {
+      alert("no privou");
+    } else {
+      let previosQuestion = currentQuestionIndex - 1;
+      setCurrentQuestionIndex(previosQuestion);
+      setCurrentQuestion(question[previosQuestion]);
+      setAnswer(question[previosQuestion].answer);
+      setNumberQuestion(numberQuestion + 1);
+    }
+  };
   const onHandingClick = (e) => {
-    if (e.target.innerHTML === answer) {
+    showHiddenOption();
+
+      if (e.target.innerHTML === answer) {
       setTimeout(() => {
         document.getElementById("correctQuestionSound").play();
       }, 500);
@@ -106,76 +105,134 @@ const previosQuestion =() =>{
     // setNumberOfAnswerDo(numberOfAnswerDo + 1),
     // console.log(currentQuestionIndex)
   );
+  const onHintClick = () => {
+    if (Hint > 0) {
+      const options = Array.from(document.querySelectorAll(".quizBox_option"));
+    let correctAnswerIndexNo;
+    options.map((option, i) => {
+      if (option.innerHTML.toLocaleLowerCase() === answer.toLocaleLowerCase()) {
+        correctAnswerIndexNo = i;
+        console.log(correctAnswerIndexNo);
+      }
+    });
+    while (true) {
+      const randomNo = Math.round(Math.random() * 3);
+      if (randomNo !== correctAnswerIndexNo && !(priviosRandomNo.includes(randomNo)) ) {
+        options.map((option, i) => {
+          if (i === randomNo) {
+            option.style.visibility = "hidden";
+            console.log("claSS ");
+            setHint(Hint-1)
+            setPriviosRandomNo(priviosRandomNo.concat(randomNo))
+          }
+        });
+      break;
 
+      }
+      if (priviosRandomNo.length >= 3 ) {
+        break;
+      }
+    }
+    }
+  };
+
+  const showHiddenOption = ()=>{
+    setPriviosRandomNo([])
+    const options = Array.from(document.querySelectorAll(".quizBox_option"));
+    options.map((option, i) => {
+      option.style.visibility = "visible";
+
+      // if (option.innerHTML.toLocaleLowerCase() === answer.toLocaleLowerCase()) {
+      //   console.log(123456);
+
+      //   // correctAnswerIndexNo = i;
+      //   // console.log(correctAnswerIndexNo);
+      // }
+    });
+  }
   return (
-   <div className='container'>
-      <div  >
-      <audio id="correctQuestionSound" src={CorrectQuestionSound}></audio>
-      <audio id="wrongQuestionSound" src={WrongQuestionSound}></audio>
-      <audio id="buttonQuestionSound" src={ButtonQuestionSound}></audio>
-      <div className="quizBox_container">
-        <div className="quizBox_icon_div">
-          <p className="quizBox_ion">
-            <span>
-              {/* //fifty fity icon */}
-              <Brightness4Icon htmlColor="green" />
-            </span>
-            <span className="quizBox_hint">{FityFity}</span>
-          </p>
-          <p>
-            <span>
-              {/* hint icon */}
-              <EmojiObjectsIcon htmlColor="orange" />
-              <span className="quizBox_hint">{Hint}</span>
-            </span>
-          </p>
-        </div>
-        <div className="quizBox_icon_div">
-          <p>
-            <span>{currentQuestionIndex + 1} of 15 </span>
-          </p>
-          <p>
-            {" "}
-            <span>
-              <TimerIcon />
-            </span>
-            <span className="quizBox_hint">2.55</span>
-          </p>
-        </div>
-        {/* <div></div> */}
-        <h2 className="quizBox_question">{currentQuestion.question}</h2>
-        <div className="quizBox_answer">
-          <div className="quizBox_answer_left">
-            <p className="quizBox_option" onClick={onHandingClick}>
-              {currentQuestion.optionA}
+    <div className="container">
+      <div>
+        <audio id="correctQuestionSound" src={CorrectQuestionSound}></audio>
+        <audio id="wrongQuestionSound" src={WrongQuestionSound}></audio>
+        <audio id="buttonQuestionSound" src={ButtonQuestionSound}></audio>
+        <div className="quizBox_container">
+          <div className="quizBox_icon_div">
+            <p className="quizBox_ion">
+              <span>
+                {/* //fifty fity icon */}
+                <Brightness4Icon htmlColor="green" />
+              </span>
+              <span className="quizBox_hint">{FityFity}</span>
             </p>
-            <p className="quizBox_option" onClick={onHandingClick}>
-              {currentQuestion.optionB}
+            <p onClick={onHintClick}>
+              <span>
+                {/* hint icon */}
+                <EmojiObjectsIcon htmlColor="orange" />
+                <span className="quizBox_hint">{Hint}</span>
+              </span>
             </p>
           </div>
-          <div className="quizBox_answer_right">
-            <p className="quizBox_option" onClick={onHandingClick}>
-              {currentQuestion.optionC}
+          <div className="quizBox_icon_div">
+            <p>
+              <span>{currentQuestionIndex + 1} of 15 </span>
             </p>
-            <p className="quizBox_option" onClick={onHandingClick}>
-              {currentQuestion.optionD}
+            <p>
+              {" "}
+              <span>
+                <TimerIcon />
+              </span>
+              <span className="quizBox_hint">2.55</span>
             </p>
           </div>
+          {/* <div></div> */}
+          <h2 className="quizBox_question">{currentQuestion.question}</h2>
+          <div className="quizBox_answer">
+            <div className="quizBox_answer_left">
+              <p className="quizBox_option" onClick={onHandingClick}>
+                {currentQuestion.optionA}
+              </p>
+              <p className="quizBox_option" onClick={onHandingClick}>
+                {currentQuestion.optionB}
+              </p>
+            </div>
+            <div className="quizBox_answer_right">
+              <p className="quizBox_option" onClick={onHandingClick}>
+                {currentQuestion.optionC}
+              </p>
+              <p className="quizBox_option" onClick={onHandingClick}>
+                {currentQuestion.optionD}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="quizBox_button">
-        <button  class="btn waves-effect waves-light " id='previos' onClick={onButtonClick} contant="Previos">
-          Previos
-        </button>
-        <button  class="btn waves-effect waves-light " id='next' onClick={onButtonClick} contant="next">
-          Next
-        </button>
-        <button  class="btn waves-effect waves-light" id='submit' onClick={onButtonClick} contant="submit">
-          submit
-        </button>
+        <div className="quizBox_button">
+          <button
+            class="btn waves-effect waves-light "
+            id="previos"
+            onClick={onButtonClick}
+            contant="Previos"
+          >
+            Previos
+          </button>
+          <button
+            class="btn waves-effect waves-light "
+            id="next"
+            onClick={onButtonClick}
+            contant="next"
+          >
+            Next
+          </button>
+          <button
+            class="btn waves-effect waves-light"
+            id="submit"
+            onClick={onButtonClick}
+            contant="submit"
+          >
+            submit
+          </button>
+        </div>
       </div>
     </div>
- 
-   </div>
- );
+  );
 }
